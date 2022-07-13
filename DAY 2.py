3c2aa25fea694cf22fa492958759cac8408c5f3f
alobaidy <hssain1233@gmail.com>
2022-07-13 21:33:09 +0300

    new

---
+++ DAY 2.py
@@ -0,0 +1,12 @@
+# # BMI CALCULATOR
+# height = input('what is your height in m: ')
+# weight = input("what is your weight in kg: ")
+# bmi = float(weight)/float(height)**2
+# BMI = int(bmi)
+# print(BMI)
+# the round function rounds floats for example
+print(round(8/3)) # round to nearst ten
+print(round(8/3, 2)) # round to two decimal points
+print(round(8/3, 3)) # round to three decimal points and so on
+print(round(22/7, 15))
+
